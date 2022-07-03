eada04323e0409c48120fb743ebffa6d1900b78d
ahza64 <justinmichaelpettit@gmail.com>
2022-07-03 11:55:29 -0700

    filter expenses with filter method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,10 +11,14 @@ export default function Expenses(props) {
     setFilteredYear(selectedYear)
   }
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map((expense) => {
+      {filteredExpenses.map((expense) => {
         return <ExpenseItem
           key={expense.id}
           title={expense.title}

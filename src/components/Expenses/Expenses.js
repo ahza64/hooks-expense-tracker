634c4601d57dc65563b3c372f65dfcf2075f4615
ahza64 <justinmichaelpettit@gmail.com>
2022-07-03 22:18:11 -0700

    Chart with dynamic styling

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 import './Expenses.css'
 
 export default function Expenses(props) {
@@ -21,6 +22,7 @@ export default function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList expenses={filteredExpenses} />
     </Card>
   )
